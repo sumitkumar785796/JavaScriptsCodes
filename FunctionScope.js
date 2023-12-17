8f1db121a18394fbf28914f34a0103814f7747fa
sumitkumar785796 <sumitkumar785796@gmail.com>
2023-12-17 15:24:52 +0530

    change some code

---
+++ FunctionScope.js
@@ -0,0 +1,20 @@
+function myVar(){
+    var a=2
+    console.log(a)
+}
+
+function myLet(){
+    let a=4
+    console.log(a)
+}
+
+function myConst(){
+    const a=5
+    console.log(a)
+}
+
+// console.log(a)//ReferenceError: a is not defined
+
+myVar()
+myLet()
+myConst()
---
+++ GlobalScope.js
@@ -0,0 +1,19 @@
+//All function are declared in global variable
+var x=1
+function myVar(){
+    console.log(x)
+}
+myVar()
+console.log(x)
+let y=2
+function myLet(){
+    console.log(y)
+}
+myLet()
+console.log(y)
+const z=3
+function myConst(){
+    console.log(z)
+}
+myConst()
+console.log(z)
