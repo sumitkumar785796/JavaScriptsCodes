ea3a5deca11df75425dd266d3e8220e0e66de582
sumitkumar785796 <sumitkumar785796@gmail.com>
2023-12-17 13:46:04 +0530

    change some code

---
+++ Blockscope.js
@@ -0,0 +1,17 @@
+// Block scope
+{
+    let x=3
+}
+// console.log(x)//ReferenceError: x is not defined
+{
+    const x=5
+}
+// console.log(x)//ReferenceError: x is not defined
+{
+    var x=8
+}
+console.log(x)
+{
+    x=6
+}
+console.log(x)
--- array.js
+++ array.js
@@ -1,3 +1,4 @@
 const a=[3,4,5,6]
-a[5]=5
+a[0]=1//change or update
+a[4]=4//add or insert
 console.log(a)
