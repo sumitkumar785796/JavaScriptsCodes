8c7fd150434e7801da76d4f6f643034e52ff89b2
sumitkumar785796 <sumitkumar785796@gmail.com>
2023-12-17 16:57:18 +0530

    first commit

---
+++ SpreadOperator.js
@@ -0,0 +1,7 @@
+const array1=[1,2,3,4]
+const array2=[...array1,5,6,7]
+const array3=[1,2,3,...array1,5,6,7]
+const array4=[1,2,3,4,5,6,7,...array1]
+console.log(array2)
+console.log(array3)
+console.log(array4)
