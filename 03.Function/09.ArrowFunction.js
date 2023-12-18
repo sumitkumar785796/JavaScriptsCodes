26c8eb761087016503b32db1703492796e34ee2a
sumitkumar785796 <sumitkumar785796@gmail.com>
2023-12-18 11:22:26 +0530

    first commit

--- 03.Function/09.ArrowFunction.js
+++ 03.Function/09.ArrowFunction.js
@@ -11,3 +11,14 @@ Hello();
 //pass argument in arrow function
 const square = (x) => x * x;
 console.log(square(4))
+//create multiple statement in arrow function.It can use the cruly braces `{}`
+const stud = (n) => {
+    if(n%2==0){
+        return "Even"
+    } else{
+        return "ODD"
+    }
+
+}
+const result = stud(221) 
+console.log(result)
