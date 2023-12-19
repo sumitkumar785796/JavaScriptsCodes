914c7762d16629af22ac5aa9c261eeff7bb5345f
sumitkumar785796 <76932212+sumitkumar785796@users.noreply.github.com>
2023-12-19 15:45:07 +0530

    add codes

--- 03.Function/10.CallbackFunction.js
+++ 03.Function/10.CallbackFunction.js
@@ -2,6 +2,19 @@
 A callback function is a function passed as an argument 
 to another function and is invoked after some task is complete.
 */
+function sayHello(){
+    console.log('Hello...')
+}
+function sayHi(){
+    console.log('Hi...')
+}
+function twoNumber(a,b,callFun){
+    callFun();
+    return a+b;
+}
+console.log(twoNumber(2,3,sayHello))
+console.log(twoNumber(2,3,sayHi))
+
 const add=(a,b)=>console.log(a+b);
 const sub=(a,b)=>console.log(a-b);
 const div=(a,b)=>console.log(a/b);
