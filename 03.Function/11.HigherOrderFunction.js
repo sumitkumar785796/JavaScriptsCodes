d61aa46dbc56884db080715fa2f2f42cfa81da29
sumitkumar785796 <76932212+sumitkumar785796@users.noreply.github.com>
2023-12-19 16:03:34 +0530

    first commit

---
+++ 03.Function/11.HigherOrderFunction.js
@@ -0,0 +1,17 @@
+// A function that takes one or more functions as arguments or returns a function.
+
+// Your function definition
+function multiplier(factor) {
+    return function (x) {
+      return x * factor;
+    };
+}
+
+// Create a new function with factor = 2
+let double = multiplier(2);
+
+// Call the new function with an argument (e.g., 5)
+let result = double(5);
+
+// Output the result
+console.log(result); // Output: 10
