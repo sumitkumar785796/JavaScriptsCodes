f93b7fdb61bf17ba1f8fe4a07ac96f963e6a0f0c
sumitkumar785796 <76932212+sumitkumar785796@users.noreply.github.com>
2023-12-19 13:13:29 +0530

    add code

---
+++ 06.Objects/01.ExampleObject.js
@@ -0,0 +1,18 @@
+const stud={
+    fname:'sumit',
+    lname:'kumar',
+    age:21,
+    skill:['nodejs','javascript'],  
+    address:{
+        distict:'Jehanabad',
+        state:'Bihar',
+        pincode:804408,
+    },
+    fullname:function(){
+        return this.fname +" "+ this.lname;
+    },
+}
+console.log(stud.fname)
+console.log(stud.fullname())
+console.log(stud.skill[0])
+console.log(stud.address.distict)
