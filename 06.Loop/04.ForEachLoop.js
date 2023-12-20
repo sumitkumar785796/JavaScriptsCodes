6e26584b76f48cf9c02711e1c76bd42fc3068545
sumitkumar785796 <76932212+sumitkumar785796@users.noreply.github.com>
2023-12-20 17:07:32 +0530

    first commit

---
+++ 06.Loop/04.ForEachLoop.js
@@ -0,0 +1,4 @@
+const array=[1,2,3,4,5,6,7,8]
+array.forEach(function (result) {
+    console.log(result);
+});
---
+++ 06.Loop/08.Continue.js
@@ -0,0 +1,7 @@
+for (let i = 0; i < 5; i++) {
+    if (i === 2) {
+      continue;
+    }
+    console.log(i);
+}
+  
