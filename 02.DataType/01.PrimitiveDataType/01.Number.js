438e3ee24a07f4a7238868405f29b164a92e8068
sumitkumar785796 <76932212+sumitkumar785796@users.noreply.github.com>
2023-12-20 13:54:01 +0530

    first commit

--- 02.DataType/01.PrimitiveDataType/01.Number.js
+++ 02.DataType/01.PrimitiveDataType/01.Number.js
@@ -1,7 +1,17 @@
 /*
 Represents numeric values (integers or floating-point numbers).
 */
+//integer
 let a=9;
+//float
 let f=3.142;
-console.log(a);
-console.log(f);
+console.log(a);// output:9
+console.log(f);//output:3.142
+//Exponential notation
+let y =123e5;
+let z=123e-5;
+console.log(y);//output:12300000
+console.log(z);//output:0.00123
+//BigInt
+let x = BigInt("123456789012345678901234567890");
+console.log(x);//output:123456789012345678901234567890n
