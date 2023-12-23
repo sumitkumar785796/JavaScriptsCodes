da5c9d37ee83a43f1c8f9cc02da3006815e68b46
sumitkumar785796 <76932212+sumitkumar785796@users.noreply.github.com>
2023-12-23 19:52:14 +0530

    update some code

--- 11.Asynchronous/02.callback.js
+++
@@ -1,9 +0,0 @@
-// callback function
-function fetchData(callback) {
-    // Simulating an asynchronous operation 
-    setTimeout(() => {
-      const data = "Some data";
-      callback(data);
-    }, 1000);
-}
-fetchData((result) => console.log(result))
